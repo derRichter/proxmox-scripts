bdbea99d62d4cb3cb31bf7ba4de01416ed73d9d8
derRichter <mailto@derrichter.de>
2026-03-17 12:07:14 +0100

    Update install.sh

--- apps/nginx-proxy-manager/install.sh
+++ apps/nginx-proxy-manager/install.sh
@@ -215,7 +215,8 @@ step_start "Nginx Proxy Manager" "Downloading" "Downloaded"
   rm -rf /app/* 
   NPM_VERSION=$(os_fetch -O- https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
   os_fetch -O- https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v$NPM_VERSION | tar -xzf - -C /app --strip-components=1
-  cd ./nginx-proxy-manager-$NPM_VERSION
+  cd /app
+  #cd ./nginx-proxy-manager-$NPM_VERSION
   step_end "Nginx Proxy Manager ${CLR_CYB}v$NPM_VERSION${CLR} ${CLR_GN}Downloaded"
 
 step_start "Enviroment" "Setting up" "Setup"
